7e8955ba9cf7e9a4bcf3f92e7a0694e989fe50f8
Lukas Heeb <lu.heeb@gmail.com>
2022-12-31 14:24:28 +0100

    setup angular app

---
+++ froeschli-ui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss', './simple-grid.scss']
+})
+export class AppComponent {
+  title = 'froeschli-ui';
+}

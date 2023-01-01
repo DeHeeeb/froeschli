2700fd5e3e426e3c7ecba9d2bf72f67e9da51a8e
Lukas Heeb <lu.heeb@gmail.com>
2023-01-01 22:08:28 +0100

    added basic weather overview

--- froeschli-ui/src/app/app.component.ts
+++ froeschli-ui/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss', './simple-grid.scss']
+  styleUrls: ['./simple-grid.scss', './app.component.scss']
 })
-export class AppComponent {
-  title = 'froeschli-ui';
+export class AppComponent implements OnInit {
+  public weatherBayreuth: any;
+  public weatherStGallen: any;
+
+  private baseUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&lang=de&APPID=fbb4bb9ce8ee1b0e9097972e8429c9d3";
+  private bayreuthUrl = this.baseUrl + "&q=Bayreuth,de&";
+  private stGallenUrl = this.baseUrl + "&q=Sankt Gallen,ch&";
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.getBayreuth().subscribe(bayreuth =>
+      this.weatherBayreuth = bayreuth);
+    this.getStGallen().subscribe(stGallen =>
+      this.weatherStGallen = stGallen);
+  }
+
+  getBayreuth() {
+    return this.http.get<any>(this.bayreuthUrl);
+  }
+
+  getStGallen() {
+    return this.http.get<any>(this.stGallenUrl);
+  }
 }
